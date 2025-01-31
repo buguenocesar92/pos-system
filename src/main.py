de5dff55cbd9409daf5bcf0221fe4ae3c5566229
buguenocesar92 <buguenocesar92@gmail.com>
2025-01-31 18:21:22 -0300

    Refactor comments for clarity in main.py regarding host configuration window logic

--- src/main.py
+++ src/main.py
@@ -1,20 +1,22 @@
+# main.py
 import sys
 import os
+
 from PyQt6.QtWidgets import QApplication
+from constants import HOST_FILE
 from windows.host_config import HostConfigWindow
 from windows.login import LoginWindow
-from constants import HOST_FILE
 
 def main():
     app = QApplication(sys.argv)
 
     # Verificar si el archivo de configuración del host existe
     if os.path.exists(HOST_FILE):
-        # Si existe, abrir la ventana de inicio de sesión directamente
+        # Si existe, abrir la ventana de inicio de sesión
         login_window = LoginWindow()
         login_window.show()
     else:
-        # Si no existe, abrir la ventana de configuración del host
+        # Si no existe, abrir la ventana de configuración de host
         host_config_window = HostConfigWindow()
         host_config_window.show()
 
