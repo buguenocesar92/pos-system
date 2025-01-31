6b818b049c2400c65948a17f024523cfe0ceda24
buguenocesar92 <buguenocesar92@gmail.com>
2025-01-31 18:21:15 -0300

    Update constants for token and host configuration file paths

--- src/constants.py
+++ src/constants.py
@@ -1,3 +1,11 @@
+# constants.py
+
+# URL base de tu API:
 API_BASE_URL = "http://localhost:8000/api"
-TOKEN_FILE = "auth_token.txt"
+
+# Archivos donde guardarás los tokens:
+ACCESS_TOKEN_FILE = "access_token.txt"
+REFRESH_TOKEN_FILE = "refresh_token.txt"
+
+# Archivo para guardar el Host:
 HOST_FILE = "host_config.txt"
